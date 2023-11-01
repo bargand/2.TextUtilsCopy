19fa724fd105b3589d120384d1b1a4446d5e827b
Debargha Nandi <debarghanandi.india@gmail.com>
2023-11-01 20:42:03 +0530

    modify the CLS in upper alert

--- src/cpmponents/Alert.js
+++ src/cpmponents/Alert.js
@@ -7,14 +7,18 @@ export default function Alert(props) {
     return lower.charAt(0).toUpperCase() + lower.slice(1);
   };
   return (
-    props.alert && (
-      <div
-        className={`alert alert-${props.alert.type} alert-dismissible fade show`}
-        role="alert"
-      >
-        {capitalized(props.alert.type)}
-        <strong>: {props.alert.msg}</strong>
+    <>
+      <div style={{ height: "50px" }}>
+        {props.alert && (
+        <div
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          {capitalized(props.alert.type)}
+          <strong>: {props.alert.msg}</strong>
+        </div>
+        )}
       </div>
-    )
+    </>
   );
 }
